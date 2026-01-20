f13732c5c2d2a677ef976ecfd0d94a08ba4e225a
AxC1271 <axc1271@case.edu>
2026-01-20 11:11:42 -0500

    added control unit

--- rtl-design/riscv-processor/control-unit/control_unit.sv
+++ rtl-design/riscv-processor/control-unit/control_unit.sv
@@ -35,7 +35,99 @@ module control_unit (
     localparam F7_ADD = 7'b0000000;
     localparam F7_SUB = 7'b0100000;
 
+    initial begin
+        RegWrite <= 1'b0;
+        MemRead <= 1'b0;
+        MemWrite <= 1'b0;
+        BranchEq <= 1'b0;
+        MemToReg <= 1'b0;
+        ALUSrc <= 1'b0;
+        ALUCont <= 1'b0;
+        JMP <= 1'b0;
+    end
+
     always_comb begin
+        case (opcode)
+            OP_R_TYPE: begin
+                RegWrite <= 1'b1;
+                MemRead <= 1'b0;
+                MemWrite <= 1'b0;
+                BranchEq <= 1'b0;
+                MemToReg <= 1'b0;
+                ALUSrc <= 1'b0;
+                JMP <= 1'b0;
+                case (funct3)
+                    F3_ADD_SUB: begin
+                        case (funct7) 
+                            F7_ADD: ALUCont <= 3'b000; // ADD
+                            F7_SUB: ALUCont <= 3'b001; // SUB
+                        endcase
+                    end
+                    F3_AND: ALUCont <= 3'b010; // AND
+                    F3_OR: ALUCont <= 3'b011;  // OR
+                    F3_XOR: ALUCont <= 3'b100; // XOR
+                    default: ALUCont <= 3'b000; // just make add default
+                endcase
+            end
+            OP_I_ARITH: begin
+                RegWrite <= 1'b1;
+                MemRead <= 1'b0;
+                MemWrite <= 1'b0;
+                BranchEq <= 1'b0;
+                MemToReg <= 1'b0;
+                ALUSrc <= 1'b1;
+                ALUCont <= 3'b000;
+                JMP <= 1'b0;
+            end
+            OP_LOAD: begin
+                RegWrite <= 1'b1;
+                MemRead <= 1'b1;
+                MemWrite <= 1'b0;
+                BranchEq <= 1'b0;
+                MemToReg <= 1'b1;
+                ALUSrc <= 1'b1;
+                ALUCont <= 3'b000;
+                JMP <= 1'b0;
+            end
+            OP_STORE: begin
+                RegWrite <= 1'b0;
+                MemRead <= 1'b0;
+                MemWrite <= 1'b1;
+                BranchEq <= 1'b0;
+                MemToReg <= 1'b0;
+                ALUSrc <= 1'b1;
+                ALUCont <= 3'b000;
+                JMP <= 1'b0;
+            end
+            OP_BRANCH: begin
+                RegWrite <= 1'b0;
+                MemRead <= 1'b0;
+                MemWrite <= 1'b0;
+                BranchEq <= 1'b1;
+                MemToReg <= 1'b0;
+                ALUSrc <= 1'b0;
+                ALUCont <= 3'b001;
+                JMP <= 1'b0;
+            end
+            OP_JAL: begin
+                RegWrite <= 1'b1;
+                MemRead <= 1'b0;
+                MemWrite <= 1'b0;
+                BranchEq <= 1'b0;
+                MemToReg <= 1'b0;
+                ALUSrc <= 1'b1;
+                JMP <= 1'b1;
+            end
+            OP_JALR: begin
+                RegWrite <= 1'b1;
+                MemRead <= 1'b0;
+                MemWrite <= 1'b0;
+                BranchEq <= 1'b0;
+                MemToReg <= 1'b0;
+                ALUSrc <= 1'b1;
+                JMP <= 1'b1;
+            end
+        endcase
     end
 
 
