491d2bb546d4267c2bfc5b7db9579fa76b3f79f5
AxC1271 <axc1271@case.edu>
2026-01-20 11:27:10 -0500

    finished program counter and data memory

--- rtl-design/riscv-processor/data_memory/data_memory.sv
+++ rtl-design/riscv-processor/data_memory/data_memory.sv
@@ -1,7 +1,46 @@
 `timescale 1ns / 1ps
 
 module data_memory (
+    input logic clk,
+    input logic rst_n,
+    input logic[31:0] mem_addr,
+    input logic[31:0] wr_data,
 
+    // control unit flags
+    input logic MemWrite,
+    input logic MemRead,
+    output logic[31:0] rd_data
 );
+
+    logic[31:0] mem [0:255];
+
+    // set default values to zero
+    initial begin
+        for (int i = 0; i < 256; i++) begin
+            mem[i] <= 32'h0;
+        end
+    end
+
+    // handle writes
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            for (int i = 0; i < 256; i++) begin
+                mem[i] <= 32'h0;
+            end
+        end else begin
+            if (MemWrite) begin
+                memory[mem_addr] <= wr_data;
+            end
+        end
+    end
+
+    // handle reads
+    always_ff @(posedge clk) begin
+        if (MemRead) begin
+            rd_data <= mem[mem_addr];
+        end else begin
+            rd_data <= 32'b0;
+        end
+    end
 
 endmodule
--- rtl-design/riscv-processor/program-counter/program_counter.sv
+++ rtl-design/riscv-processor/program-counter/program_counter.sv
@@ -2,9 +2,15 @@
 
 module program_counter (
     input logic clk,
-    input logic rst_n,
     input logic[31:0] pc_in,
     output logic[31:0] pc_out
 );
+    // unlike the previous design, rst
+    // is removed from the program counter
+    // the instruction reset will be handled
+    // in the top module
+    always_ff @(posedge clk) begin
+        pc_out <= pc_in;
+    end
 
 endmodule
