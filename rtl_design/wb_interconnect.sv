2bb24137239836411817f6a1d74c454b3e34cdb2
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2026-07-22 22:26:46 -0700

    working on wishbone interconnect

--- rtl_design/wb_interconnect.sv
+++ rtl_design/wb_interconnect.sv
@@ -1,5 +1,27 @@
 module wb_interconnect (
-
+    // ports from master wrapper
+    input  logic        mstr_cycle,
+    input  logic        mstr_strb, 
+    input  logic        mstr_we,
+    input  logic [31:0] mstr_addr,
+    input  logic [31:0] mstr_dat_w, 
+    input  logic [3:0]  mstr_sel,
+    output logic [31:0] mstr_dat_r,
+    output logic        mstr_ack,
+    // decode ports to peripherals
+    // connects the cpu to four peripherals:
+    // ram, gpio, uart, and a timer, 5th
+    // bit is added for invalid addresses
+    output logic [4:0] slve_sel, // one hot encoded
+    
 );
+
+// how I'm doing naming conventions
+// mstr_ means the signals connect
+// straight to the master port, but
+// doesn't necessarily mean master 
+// drives it. mstr_ack, for example,
+// is an acknowledge signal from a
+// slave sent TO the master
 
 endmodule
--- rtl_design/wb_master.sv
+++ rtl_design/wb_master.sv
@@ -7,11 +7,11 @@ module wb_master (
     input  logic        dmem_wr_en,
     output logic [31:0] dmem_rdata,
     output logic        dmem_ready,
-    // master output ports
+    // interconnect interface
     output logic        wb_cycle, // bus cycle in progress
-    output logic        wb_stb,   // this current transfer is valid
+    output logic        wb_strb,  // this current transfer is valid
     output logic        wb_we,    // 1 = write, 0 = read
-    output logic [31:0] wb_adr,   // byte address
+    output logic [31:0] wb_addr,  // byte address
     output logic [31:0] wb_dat_w, // write data
     output logic [3:0]  wb_sel,   // byte lane enable
     input  logic [31:0] wb_dat_r, // read data
@@ -20,8 +20,8 @@ module wb_master (
 
     // let's just do the easy assigns first on the wishbone side
     assign wb_cycle = (dmem_rd_en || dmem_wr_en);
-    assign wb_stb   = (dmem_rd_en || dmem_wr_en);
-    assign wb_adr   = dmem_addr;
+    assign wb_strb  = (dmem_rd_en || dmem_wr_en);
+    assign wb_addr  = dmem_addr;
     assign wb_dat_w = dmem_wdata;
     assign wb_sel   = dmem_wstrb;
     assign wb_we    = dmem_wr_en;
