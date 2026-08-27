eb5cc23d0f3dd3a138e268074559cd34ad6d3f4c
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2026-08-27 17:52:30 -0400

    increased timing to 58.8 MHz

--- 0-rtl/core_riscv.sv
+++ 0-rtl/core_riscv.sv
@@ -1,7 +1,7 @@
 // 5-stage in-order RV32I core, CACHELESS (Harvard, tightly-coupled memory).
 // IF -> ID -> EX -> MEM -> WB
 // fetch and data go straight to memory
-module core_riscv_pipeline # (
+module core_riscv # (
     parameter logic [31:0] RESET_VECTOR = 32'h0000_0000
 )(
     input  logic clk,
